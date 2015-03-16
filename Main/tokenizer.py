3b7369a27b714c9f192ea16fcf2f612f8576b800
Raphael Halff <raphi.halff@gmail.com>
2015-03-16 19:41:33 +0200

    Alternate file option added to tokenizer, print option added, be cautious of file length

--- Main/tokenizer.py
+++ Main/tokenizer.py
@@ -18,7 +18,15 @@ Attempt to tokenize excerpt of Talmud using NLTK
 
 
 path=""
-if len(sys.argv)==2:
+to_print = True
+if len(sys.argv)==3:
+    if os.path.exists(sys.argv[1]):
+        path = sys.argv[1]
+        if sys.argv[2].lower()=="n":
+            to_print = False
+    else:
+        print "usage: python tokenizer.py [/path/|d=default] [print output?: y|n]" #what is the proper usage notation?
+elif len(sys.argv)==2:
     if os.path.exists(sys.argv[1]):
         path = sys.argv[1]
     else:
@@ -33,19 +41,22 @@ f = codecs.open(path, encoding='utf8')
     print(line)"""
 
 raw = f.read()
-print 'Raw Text:\n'
-print raw
+if to_print:
+    print 'Raw Text:\n'
+    print raw
 
 tokens = word_tokenize(raw)
 fdist = nltk.FreqDist(tokens)
 sorted(fdist)
 
-print '\nTokens:\n'
-for key in fdist:
-    print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
-print '\n\nUnique words: \n' 
-for w in  fdist.hapaxes():
-    print w.encode('utf8') + '; ',
+
+if to_print:
+    print '\nTokens:\n'
+    for key in fdist:
+        print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
+    print '\n\nUnique words: \n' 
+    for w in  fdist.hapaxes():
+        print w.encode('utf8') + '; ',
 
 print '\n\nPlotting...' #would like to switch y and x axis
 fdist.plot(title='Tokenization: Word Frequency')
