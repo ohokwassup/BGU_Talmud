4292ee2e12bba96f365b96daf130f5b082185d0f
Raphael Halff <raphi.halff@gmail.com>
2015-04-15 18:06:24 +0300

    pickles and unpickles seems efficient, semantic problem

--- Main/tokenizer.py
+++ Main/tokenizer.py
@@ -32,71 +32,82 @@ elif len(sys.argv)==2:
     else:
         print "usage: python tokenizer.py [/path/|d=default]" #what is the proper usage notation?
 else:
-    path = '/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/mishna_ex.txt'
+    if not os.path.exists('/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/tokens.pkl'):
+        path = '/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/talmud.txt' # or mishna_ex.txt for testing
 
-#f = codecs.open(path, encoding='utf8')
+        #f = codecs.open(path, encoding='utf8')
 
-"""for line in f:
-    line = line.strip()
-    print(line)"""
-def make_tokens():
-    with codecs.open(path, encoding='utf8') as f:
-        for line in f:
-            #for word in line.split():
-            yield word_tokenize(line) #tokenize or word_tokenize?
-#storing as freqdist
-tokens = []
-tokens = make_tokens()
-print 'printing tokens'
-print tokens
-print 'printing t in tokens'
-for t in tokens:
-    if type(t) is list:
-        print 'tks are coming'
-        for tks in t:
-            print tks.encode('utf8')
-        tokens = t
-fdist = nltk.FreqDist(tokens)
-sorted(fdist)
-fdist.plot()
-#attempt to store tokens directly from generator to freqdist
-'''fdist = nltk.FreqDist(make_tokens())
-sorted(fdist)
-'''
-#printing tokens
-for key in fdist:
-    print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
+        """for line in f:
+            line = line.strip()
+            print(line)"""
+        def make_tokens():
+            with codecs.open(path, encoding='utf8') as f:
+                for line in f:
+                    #for word in line.split():
+                    yield word_tokenize(line) #tokenize or word_tokenize?
+        #storing as freqdist
+        tokens = []
+        tokens = make_tokens()
+        print 'printing tokens'
+        print tokens
+        print 'printing t in tokens'
+        for t in tokens:
+            if type(t) is list:
+                print 'tks are coming'
+                for tks in t:
+                    print tks.encode('utf8')
+                tokens = t
+        fdist = nltk.FreqDist(tokens)
+        print fdist.N() #print how many outcomes
+        sorted(fdist)
+        #fdist.plot(10)#add integer to limit number of items plotted
+        #attempt to store tokens directly from generator to freqdist
+     """fdist = nltk.FreqDist(make_tokens())
+        sorted(fdist)
+        """
+        #printing tokens
+    """ for key in fdist:
+            print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
+        """
+        #pickling
+        outfile = open("tokens.pkl","wb")
+        pickle.dump(fdist, outfile, 1) #MUST use protocol 1, else 'FreqDist'[...]'_N' error, might be interesting to know why 
+        outfile.close()
 
-#pickling
-outfile = open("tokens.pkl","wb")
-pickle.dump(fdist, outfile, 1) #MUST use protocol 1, else 'FreqDist'[...]'_N' error, might be interesting to know why 
-outfile.close()
+        #test unpickling
+        infile = open("tokens.pkl", "rb")
+        dist = pickle.load(infile)
+        infile.close()
+        #dist.plot()
+        """
+        raw = f.read()
+        if to_print:
+            print 'Raw Text:\n'
+            print raw
+        """
+        #tokens = word_tokenize(raw)
+        """
+        fdist = nltk.FreqDist(tokens)
+        sorted(fdist)
+
+
+        if to_print:
+            print '\nTokens:\n'
+            for key in fdist:
+                print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
+            print '\n\nUnique words: \n' 
+            for w in  fdist.hapaxes():
+                print w.encode('utf8') + '; ',
+
+        print '\n\nPlotting...' #would like to switch y and x axis
+        fdist.plot(title='Tokenization: Word Frequency')
+        """
 
 #test unpickling
+print 'STARTING THE UNPICKLED FILE'
 infile = open("tokens.pkl", "rb")
 dist = pickle.load(infile)
 infile.close()
-#dist.plot()
-"""
-raw = f.read()
-if to_print:
-    print 'Raw Text:\n'
-    print raw
-"""
-#tokens = word_tokenize(raw)
-"""
-fdist = nltk.FreqDist(tokens)
-sorted(fdist)
-
-
-if to_print:
-    print '\nTokens:\n'
-    for key in fdist:
-        print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
-    print '\n\nUnique words: \n' 
-    for w in  fdist.hapaxes():
-        print w.encode('utf8') + '; ',
-
-print '\n\nPlotting...' #would like to switch y and x axis
-fdist.plot(title='Tokenization: Word Frequency')
-"""
+print dist.N() #print how many outcomes
+sorted(dist)
+dist.plot(10)#add integer to limit number of items plotted
