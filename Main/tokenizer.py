0037a953f6a439bd512f62f7317229f512a15921
Raphael Halff <raphi.halff@gmail.com>
2015-04-27 12:13:26 +0300

    pickling now makes two files (one for distributions one for tokens), tokenizing is modified to treat abbreviated name as one word

--- Main/tokenizer.py
+++ Main/tokenizer.py
@@ -5,6 +5,7 @@ from __future__ import division  # Python 2 users only
 import nltk, re, pprint
 from nltk import tokenize, word_tokenize, probability
 from nltk.text import ConcordanceIndex 
+from nltk.tokenize import RegexpTokenizer
 import codecs
 from pylab import *
 import sys
@@ -30,11 +31,12 @@ if not os.path.exists('/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/tokens.
     """for line in f:
         line = line.strip()
         print(line)"""
+    tokenizer = RegexpTokenizer("""(\S+"\S+)|\s?[\u05D0-\u05EA]+\s|(\.|\\|\?|\(|\)|:|;)|([\u05D0-\u05EA]'\s)""")
     def make_tokens():
         with codecs.open(path, encoding='utf8') as f:
             for line in f:
                 #for word in line.split():
-                yield word_tokenize(line) #tokenize or word_tokenize?
+                yield tokenizer.tokenize(line) #word_tokenize(line) #tokenize or word_tokenize?
     #storing as freqdist
     tokens = []
     tokens = make_tokens()
@@ -53,7 +55,7 @@ if not os.path.exists('/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/tokens.
         else:#this wont happen, tokenizer is returning list of tokens by line, every line is a list
             tokenslist.append(t)
             count = count + 1
-    print count + ' tokens'
+    print str(count) + ' tokens'
     fdist = nltk.FreqDist(tokenslist)
     print fdist.N() #print how many outcomes
     sorted(fdist)
@@ -70,15 +72,12 @@ if not os.path.exists('/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/tokens.
         print key.encode('utf8') + ': ' + str(fdist[key]) + '; ',
     """
     #pickling
-    outfile = open("tokens.pkl","wb")
-    pickle.dump(fdist, outfile, 1) #MUST use protocol 1, else 'FreqDist'[...]'_N' error, might be interesting to know why 
-    outfile.close()
-
-    #test unpickling
-    infile = open("tokens.pkl", "rb")
-    dist = pickle.load(infile)
-    infile.close()
-    #dist.plot()
+    outfile_tokens = open("tokens.pkl", "wb")
+    outfile_dist = open("dist.pkl","wb")
+    pickle.dump(tokenslist, outfile_tokens, 1)
+    pickle.dump(fdist, outfile_dist, 1) #MUST use protocol 1, else 'FreqDist'[...]'_N' error, might be interesting to know why 
+    outfile_tokens.close()
+    outfile_dist.close()
     """
     raw = f.read()
     if to_print:
@@ -104,10 +103,17 @@ if not os.path.exists('/Users/Raphi/Documents/WorkThings/Talmud_Pro/Main/tokens.
     """
 #pickle file made, begin here if pickled file already existed
 #test unpickling
-print 'STARTING ON THE PICKLED FILE'
-infile = open("tokens.pkl", "rb")
-dist = pickle.load(infile)
-infile.close()
+print 'STARTING ON THE PICKLED FILES'
+infile_tokens = open("tokens.pkl", "rb")
+tokens = pickle.load(infile_tokens)
+infile_tokens.close()
+print 'TOKENS HAVE BEEN LOADED'
+
+infile_dist = open("dist.pkl", "rb")
+dist = pickle.load(infile_dist)
+infile_dist.close()
+print 'DISTRIBUTIONS HAVE BEEN LOADED'
+
 print 'number of hapaxes: ' +  str(len(dist.hapaxes()))
 print str(dist.N()) + ' outcomes' #print how many outcomes
 sorted(dist)
